349082ff800cb7db60dce047120a142bbbd8b083
Carlos Silva <carlosemilio.contato@gmail.com>
2024-09-20 21:28:26 -0300

    fix: add item

---
+++ src/components/CardItem/AddItem/index.tsx
@@ -0,0 +1,9 @@
+export function AddItem() {
+  return (
+    <div>
+      <button type="button"> </button>
+      <input type="number" name="" id="" />
+      <button type="button"> </button>
+    </div>
+  )
+}
