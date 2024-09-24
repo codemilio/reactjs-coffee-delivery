669df9466b2af85e4f7d2bdb29a78fef35e2123e
Carlos Silva <carlosemilio.contato@gmail.com>
2024-09-23 22:01:41 -0300

    feat: button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from "react"
+
+type Props  = {
+  children: ReactNode
+}
+
+export function Button({ children }: Props) {
+  
+  return (
+    <button type="button"  className="rounded-lg bg-gray-100">
+      { children}
+    </button>
+  )
+}
