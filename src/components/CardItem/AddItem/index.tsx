c9f18cbd19829212b9502bd41f981c0e0de73374
Carlos Silva <carlosemilio.contato@gmail.com>
2024-10-06 23:48:31 -0300

    chore: add item

--- src/components/CardItem/AddItem/index.tsx
+++ src/components/CardItem/AddItem/index.tsx
@@ -1,10 +1,12 @@
 import { Minus, Plus } from '@phosphor-icons/react'
+import { useState } from 'react'
 import { Button } from '../../Button'
 
 export function AddItem() {
+  const [itensNumber, setNumberItens] = useState(0)
 
   const handleAdd = () => {
-    
+    setNumberItens((prev) => prev + 1)
   }
   
   const handleSub = () => {
